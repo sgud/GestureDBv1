d72cbd7e8035f972510f419483389d7ebec4cb1d
sgud <suhas_gudhe@yahoo.com>
2015-04-20 21:34:25 -0400

    Broken Sqlite Master Query

--- GestureDBv1/Model.m
+++ GestureDBv1/Model.m
@@ -13,17 +13,42 @@
 @implementation Model
 
 + (int)getTableNum {
+    NSString *dbPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:kSQLiteName];
+    FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
+    [database open];
+    NSString *sqlSelectQuery = @"SELECT COUNT(*) FROM sqlite_master WHERE type='table'";
+//    FMResultSet *results = [database executeQuery:sqlSelectQuery];
+//    NSLog(@"RESULTS: %@",[results resultDictionary]);
+    int tableNum = [database executeQuery:sqlSelectQuery];
+//    while([results next]) {
+//        
+//    }
     // Use a query to the SQL Master table where name = "table"
     return 10;
 }
 
 + (NSString*)getTableNameForRow:(int)row {
-    return @"TableName1";
+    return @"Album";
 }
 
 + (NSArray*)getColumnNamesWithTableName:(NSString*)tableName {
+    NSString *dbPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:kSQLiteName];
+    FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
+    [database open];
+    NSString *sqlPragmaQuery = [NSString stringWithFormat:@"PRAGMA table_info(%@)",@"Album"];
+//    NSString *sqlSelectQuery = [NSString stringWithFormat:@"SELECT sql FROM sqlite_master WHERE tbl_name = '%@' AND type = 'table'",@"Album"];
     
-    return @[@"albumName", @"artist", @"artist2", @"artist3", @"artist4", @"artist5", @"artist6", @"artist7", @"artist8", @"artist9"];
+    NSMutableArray *columnNames = [[NSMutableArray alloc] initWithArray:@[]];
+    // Query result
+    FMResultSet *results = [database executeQuery:sqlPragmaQuery];
+    while([results next]) {
+        NSString *name = [results stringForColumn:@"name"];
+        [columnNames addObject:name];
+    }
+    
+    [database close];
+    NSLog(@"%@", columnNames);
+    return [NSArray arrayWithArray:columnNames];
 }
 
 + (int)getRowCountWithTableName:(NSString*)tableName {
@@ -39,29 +64,33 @@
 
 + (NSArray*)getAllData:(NSString *)tableName {
     // Getting the database path.
-    NSArray  *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *docsPath = [paths objectAtIndex:0];
-    NSString *dbPath = [docsPath stringByAppendingPathComponent:kSQLiteName];
+//    NSArray  *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+//    NSString *docsPath = [paths objectAtIndex:0];
+//    NSString *dbPath = [docsPath stringByAppendingPathComponent:kSQLiteName];
+    NSString *dbPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:kSQLiteName];
     
-    NSMutableArray *dataArray = [[NSMutableArray alloc] initWithArray:@[]];
+   
     
     FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
     [database open];
-    
-    NSString *sqlSelectQuery = @"SELECT * FROM tablename";
-
+    NSString *sqlSelect = @"SELECT * FROM ";
+    NSString *sqlSelectQuery = [sqlSelect stringByAppendingString:tableName];
+    NSMutableArray *dataArray = [[NSMutableArray alloc] initWithArray:@[]];
     // Query result
-    FMResultSet *resultsWithNameLocation = [database executeQuery:sqlSelectQuery];
-    while([resultsWithNameLocation next]) {
-        NSString *strID = [NSString stringWithFormat:@"%d",[resultsWithNameLocation intForColumn:@"ID"]];
-        NSString *strName = [NSString stringWithFormat:@"%@",[resultsWithNameLocation stringForColumn:@"Name"]];
-        NSString *strLoc = [NSString stringWithFormat:@"%@",[resultsWithNameLocation stringForColumn:@"Location"]];
+    FMResultSet *results = [database executeQuery:sqlSelectQuery];
+    while([results next]) {
+        
+        
+        
+//        NSString *strID = [NSString stringWithFormat:@"%d",[resultsWithNameLocation intForColumn:@"ID"]];
+//        NSString *strName = [NSString stringWithFormat:@"%@",[resultsWithNameLocation stringForColumn:@"Name"]];
+//        NSString *strLoc = [NSString stringWithFormat:@"%@",[resultsWithNameLocation stringForColumn:@"Location"]];
         
        
 //        dataArray addObject:<#(id)#>;
         
         // loading your data into the array, dictionaries.
-        NSLog(@"ID = %@, Name = %@, Location = %@",strID, strName, strLoc);
+//        NSLog(@"ID = %@, Name = %@, Location = %@",strID, strName, strLoc);
     }
     [database close];
     return @[];
